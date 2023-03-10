74cce37b6e97c9b046e7fdb84e38d4bb759d32a4
Abdulhameed735 <abdulhameedmoonstapha735@gmail.com>
2023-03-10 22:15:42 +0100

    Setup Tailwind, installed needed dependencies and added neccessary assets

---
+++ public/assets/images/index.js
@@ -0,0 +1,42 @@
+// Logo
+import logo from "./svg/logo.svg";
+
+// Blobs
+import blob1 from "./blobs/blob1.svg";
+import blob2 from "./blobs/blob2.svg";
+import blob3 from "./blobs/blob3.svg";
+import blob4 from "./blobs/blob4.svg";
+
+// SVGs
+import bookingKitLogo from "./svg/bookingkit logo.svg";
+import charthopLogo from "./svg/Charthop logo.svg";
+import chocoLogo from "./svg/Choco logo.svg";
+import eatFirstLogo from "./svg/Eatfirst logo.svg";
+import harnessGivingLogo from "./svg/Harness Giving logo.svg";
+import personioLogo from "./svg/Personio logo.svg";
+import upFlowLogo from "./svg/Upflow logo.svg";
+import vaiLogo from "./svg/VAI logo.svg";
+import wallboxLogo from "./svg/Wallbox logo.svg";
+
+import dropdown from "./svg/dropdown.svg";
+import lock from "./svg/lock.svg;"
+import lock2 from "./svg/lock2.svg";
+import stars from "./svg/stars.svg";
+import wallet from "./svg/wallet.svg";
+import stackcards from "./svg/Stack cards.svg";
+
+// WEBP
+import marble1 from "./webp/marbles-accelerator1.webp";
+import marble2 from "./webp/marbles-bumpy-slide.webp";
+import marble3 from "./webp/marbles-distributed.webp";
+import marble4 from "./webp/marbles-in-funnel.webp";
+import marble5 from "./webp/marbles-on-steps.webp";
+import marbele6 from "./webp/marbles-pushed-into-tube.webp"; 
+
+export default {
+    logo,
+    blob1, blob2, blob3, blob4,
+    bookingKitLogo, charthopLogo, chocoLogo, eatFirstLogo, harnessGivingLogo, personioLogo, upFlowLogo, 
+    vaiLogo, wallboxLogo, dropdown, lock, lock2, stars, wallet, stackcards,
+    marble1, marble2, marble3, marble3, marble4, marble5, marbele6
+}
