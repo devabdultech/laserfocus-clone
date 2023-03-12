3722bc59f1f51f31068a162f12aeb2fb46b8bdfe
Abdulhameed735 <abdulhameedmoonstapha735@gmail.com>
2023-03-12 12:16:38 +0100

    Finished mobile nav component

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -1,37 +1,45 @@
 import logo from "assets/images/svg/logo.svg"
-import dropdown from "assets/images/svg/dropdown.svg"
 import Image from "next/image"
 import Button from "../ui/Button"
 import CTA from "../ui/CTA"
+import MobileNav from "../ui/MobileNav"
+import Link from "next/link"
 
 const Header = () => {
   return (
-    <nav className="sticky top-0 z-10">
-      <div className="px-[0.6rem] py-2 md:px-8 md:py-4 grid md:grid-cols-[minmax(0,1fr),auto,minmax(0,1fr)] items-center z-10 relative">
-        <div className="flex gap-2 justify-start items-center">
-          <Image className="w-4 md:w-5 lg:w-6 xl:w-8" src={logo} alt='logo' />
-          <span className="font-libre font-bold leading-none text-[#30333A]">laserfocus</span>
-        </div>
-
-        <div className="hidden lg:grid grid-flow-col gap-2">
-          <Button name='Product'/>
-          <Button name='Pricing' />
-          <Button name='Blog' />
-        </div>
-
-        <div className="ml-auto pl-2 col-start-3 grid grid-flow-col gap-1 md:gap-2">
-          <div className="hidden lg:block">
-            <Button name='Log In' />
+    <nav className="">
+      <div className="sticky top-0 z-10">
+        <div className="px-[0.8rem] py-3 md:px-8 md:py-5 grid md:grid-cols-[minmax(0,1fr),auto,minmax(0,1fr)] items-center z-10 relative">
+          <Link href='/'>
+            <div className="flex gap-2 justify-start items-center">
+              <Image className="w-4 md:w-5 lg:w-6 xl:w-8" src={logo} alt='logo' />
+              <span className="font-libre font-bold leading-none text-[#30333A]">laserfocus</span>
+            </div>
+          </Link>
+          <div className="hidden lg:grid grid-flow-col gap-2">
+            <Button name='Product'/>
+            <Button name='Pricing' />
+            <Button name='Blog' />
           </div>
 
-          <CTA href='' text='Try for free' />
+          <div className="ml-auto pl-2 col-start-3 grid grid-flow-col gap-1 md:gap-2">
+            <div className="hidden lg:block">
+              <Button name='Log In' />
+            </div>
 
-          <button type="button" aria-expanded="false" className="group w-[1.9444rem] h-[1.9444rem] rounded-[0.7rem] bg-blue-100 grid place-content-center gap-[4px] transition-colors lg:hidden cursor-pointer">
-            <div className="w-[14px] h-[2px] bg-blue-500 rounded-full col-start-1 row-start-1 transition translate-y-[-0.1875rem]"></div>
-            <div className="w-[14px] h-[2px] bg-blue-500 rounded-full col-start-1 row-start-1 transition translate-y-[0.1875rem]"></div>
-          </button>
+            <CTA href='' text='Try for free' />
+
+            <button type="button" aria-expanded="false" className="group w-[1.9444rem] h-[1.9444rem] rounded-[0.7rem] bg-blue-100 grid place-content-center gap-[4px] transition-colors lg:hidden cursor-pointer">
+              <div className="w-[14px] h-[2px] bg-blue-500 rounded-full col-start-1 row-start-1 transition translate-y-[-0.1875rem]"></div>
+              <div className="w-[14px] h-[2px] bg-blue-500 rounded-full col-start-1 row-start-1 transition translate-y-[0.1875rem]"></div>
+            </button>
+          </div>
+
         </div>
-
+      </div>
+      
+      <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+        <MobileNav />
       </div>
     </nav>
   )
---
+++ components/ui/MobileNav.jsx
@@ -0,0 +1,70 @@
+import Image from 'next/image'
+
+import blob1 from "assets/images/blobs/blob1.svg"
+import blob2 from "assets/images/blobs/blob2.svg"
+import blob3 from "assets/images/blobs/blob3.svg"
+import blob4 from "assets/images/blobs/blob4.svg"
+
+
+const MobileNav = () => {
+  return (
+    <div className='h-screen pt-14'>
+        <div className='w-[90vw] md:w-[85vw] xl:w-[80vw] max-w-[66rem] mx-auto'>
+            <div className='py-6 overflow-y-auto px-1 -mx-1 h-full overscroll-contain'>
+                <div className='grid gap-6'>
+                    {/* Product */}
+                    <div className='grid gap-3'>
+                        <h2 className='font-libre text-[1.3rem] font-semibold text-primary'>Product</h2>
+                        <div className='grid gap-5'>
+                            {/* Features */}
+                            <div className='flex gap-2 font-inter font-medium text-[1.2rem] text-primary'>
+                                <Image src={blob1} alt='blob' />
+                                <span>Features</span>
+                            </div>
+
+                            {/* Solutions */}
+                            <div className='grid gap-3'>
+                                <div className='flex gap-2 font-inter font-medium text-[1.2rem] text-primary'>
+                                    <Image src={blob2} alt='blob' />
+                                    <span>Solutions</span>
+                                </div>
+
+                                <div className='grid gap-3 pl-8 text-secondary font-inter text-[1.1rem]'>
+                                    <span>Team Leads</span>
+                                    <span>FUll Cycle Sales</span>
+                                    <span>SDRs / BDRs</span>
+                                    <span>Customer Success</span>
+                                    <span>Sales Operations</span>
+                                    <span>Account Executives</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {/* Pricing */}
+                    <div className='grid gap-3'>
+                        <h2 className='font-libre text-[1.3rem] font-semibold text-primary'>Pricing</h2>
+                        <div className='flex gap-2 font-inter font-medium text-[1.2rem] text-primary'>
+                            <Image src={blob3} alt='blob' />
+                            <span>Choose your plan</span>
+                        </div>
+                    </div>
+
+                    {/* Resources */}
+                    <div className='grid gap-3'>
+                        <h2 className='font-libre text-[1.3rem] font-semibold text-primary'>Resources</h2>
+                        <div className='flex gap-2 font-inter font-medium text-[1.2rem] text-primary'>
+                            <Image src={blob4} alt='blob' />
+                            <span>Blog</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div></div>
+    </div>
+  )
+}
+
+export default MobileNav
